67f3308a656cd0293b7e9f27608c53587674e6cd
Hugo COLLIN <hugo.collin1@outlook.fr>
2024-10-02 15:19:20 +0200

    q5 not properly working

--- src/q5.py
+++ src/q5.py
@@ -3,7 +3,8 @@ from sklearn.preprocessing import LabelEncoder
 
 df = pd.read_csv('../data/music_mouv_data.csv')
 
-df_filtered = df[df['emotion'].isin(['joie', 'tension'])]
+# Filtrez les données pour ne garder que les lignes où l'émotion est 'joie' ou 'tension'
+df_filtered = df[df['emotion'].isin(['Joyful Activation', 'Tension'])]
 
 # Sélectionnez toutes les colonnes numériques sauf 'participant_id' et 'test_number'
 X = df_filtered.select_dtypes(include=['float64', 'int64']).drop(['participant_id'], axis=1)
