1a34390222d54e428c132d8aa2435797ab8cf625
Hugo COLLIN <hugo.collin1@outlook.fr>
2024-10-02 15:20:53 +0200

    q5

--- src/q5.py
+++ src/q5.py
@@ -1,5 +1,4 @@
 import pandas as pd
-from sklearn.preprocessing import LabelEncoder
 
 df = pd.read_csv('../data/music_mouv_data.csv')
 
@@ -7,11 +6,5 @@ df = pd.read_csv('../data/music_mouv_data.csv')
 df_filtered = df[df['emotion'].isin(['Joyful Activation', 'Tension'])]
 
 # Sélectionnez toutes les colonnes numériques sauf 'participant_id' et 'test_number'
-X = df_filtered.select_dtypes(include=['float64', 'int64']).drop(['participant_id'], axis=1)
-
-# Encodez les émotions en valeurs numériques
-le = LabelEncoder()
-y = le.fit_transform(df_filtered['emotion'])
-
-print("Dimensions de X:", X.shape)
-print("Dimensions de y:", y.shape)
+X = df_filtered.drop(['emotion', 'participant_id'], axis=1)
+y = df_filtered['emotion']
